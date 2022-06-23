985b91620bf28b110383c2c825a9908d65e9d7ed
richirm <richirm89@hotmail.com>
2022-06-23 15:39:12 -0500

    2 files modified

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,12 +1,12 @@
 import { Component, Input } from '@angular/core';
 
+import { PersonInterface } from '../../interfaces/Person.interface';
+
 @Component({
   selector: 'invgate-item',
   templateUrl: './item.component.html',
-  styleUrls: ['./item.component.css']
+  styleUrls: ['./item.component.css'],
 })
 export class ItemComponent {
-  
-  @Input() item;
-
+  @Input() item: PersonInterface;
 }
